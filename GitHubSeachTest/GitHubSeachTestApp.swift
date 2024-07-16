6bdc2a99b5acc4f2e42b252ce805addde754181e
Misha Deneka <mishadeneka@gmail.com>
2024-07-16 08:43:16 +0300

    Initial Commit

---
+++ GitHubSeachTest/GitHubSeachTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubSeachTestApp.swift
+//  GitHubSeachTest
+//
+//  Created by Misha Deneka on 16.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct GitHubSeachTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
